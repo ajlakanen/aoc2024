2f5e645ae8e5db03ee2f35c3d8e4e26cb6da893b
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2024-12-02 08:48:53 +0200

    day2 wip

--- 02.py
+++ 02.py
@@ -1,30 +1,81 @@
 import re
 from functools import reduce
 
-file = open('data/02-example.txt', 'r')
+file = open("data/02-example.txt", "r")
 content = file.read()
-lines = content.split('\n')
+lines = content.split("\n")
 
 items = []
 for i in range(len(lines)):
     items.append(re.split(r"\s+", lines[i]))
 
-safe = 0
 
-for i in range(len(items)):
-    nums = list(map(int, items[i]))
-    nums.reduce(lambda first: )
-    print(nums)
+def inOrder(list, decOrInc):
+    sorted = list.copy()
+    sorted.sort(reverse=decOrInc < 0)
+
+    for i in range(len(list)):
+        if list[i] != sorted[i]:
+            return False
+    return True
 
 
-# for i in range(len(items)):
-#     for j in range (len(items[i])):
-#         if j == 0:
-#             continue
-#         if abs(int(items[i][j])-int(items[i][j-1])) < 1:
-#             break
-#         if abs(int(items[i][j]) - int(items[i][j])) > 3:
-#             break
-#     safe += 1
+def part1():
+    safe = 0
+    for i in range(len(items)):
+        nums = list(map(int, items[i]))
 
-print(safe)
+        decOrInc = nums[1] - nums[0]
+
+        if not inOrder(nums, decOrInc):
+            continue
+
+        for j in range(len(nums)):
+            if j == 0:
+                continue
+            if abs(nums[j] - nums[j - 1]) < 1:
+                break
+            if abs(nums[j] - nums[j - 1]) > 3:
+                break
+            if j == len(nums) - 1:
+                safe += 1
+
+    print(safe)
+
+def decOrInc(list, ignoresLeft = 1):
+    pass
+
+def checkIfSafe(list, ignoresLeft = 1):
+    for i in range(len(list)):
+        if i == 0:
+            continue
+        if abs(list[i] - list[i - 1]) < 1:
+            if ignoresLeft == 0 or i == len(list) - 1:
+                return False            
+            else:
+                return checkIfSafe(list[:i] + list[i+1:], 0)
+
+        if abs(list[i] - list[i - 1]) > 3:
+            if ignoresLeft == 0 or i == len(list) - 1:
+                return False
+            else: 
+                return checkIfSafe(list[:i] + list[i + 1 :], 0)
+    return True
+
+def part2():
+    safe = 0
+    for i in range(len(items)):
+        nums = list(map(int, items[i]))
+
+        decOrInc = nums[1] - nums[0]
+
+        if not inOrder(nums, decOrInc):
+            continue
+
+        if checkIfSafe(nums, 1):
+            safe += 1
+
+    print(safe)
+
+part1()
+part2()
