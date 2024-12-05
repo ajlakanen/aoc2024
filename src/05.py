7c78457edf97ef80a0922ed819949c156e5ec056
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2024-12-05 11:34:41 +0200

    day 5 wip

--- src/05.py
+++ src/05.py
@@ -21,31 +21,41 @@ while i < len(lines):
 # print(updates)
 
 class Item:
-    def __init__ (self, value, after):
+    def __init__ (self, value, afters):
         self.value = value
-        self.after = after
+        self.afters = afters
 
 def part1():
-    ordering = []
-    for rule in rules:
-        if len(list(filter(lambda item: item.value == rule[0], ordering))) == 0:
-            ordering.append(Item(rule[0], [rule[1]]))
-        else:
-            item = list(filter(lambda item: item.value == rule[0], ordering))[0]
-            item.after.append(rule[1])
-    
     items = []
-    for item in ordering:
-        right = len(items)-1
-        location = 0
-        for after 
-        if right < location:
-            items.insert(location, item.value)
-            continue
-        
-        
-    # map(lambda item: print(item.value, item.after), ordering)
-    # ordering.map(lambda item: print(item.value, item.after))
-    print(items)
+    for rule in rules:
+        if len(list(filter(lambda item: item.value == rule[0], items))) == 0:
+            items.append(Item(rule[0], [rule[1]]))
+        else:
+            item = list(filter(lambda item: item.value == rule[0], items))[0]
+            item.afters.append(rule[1])
+    
+    ordering = [items[0].value]
+    
+    for item in items:
+
+        for i in range(len(ordering)):
+            newIndex = -1
+            for after in item.afters:
+                if after == ordering[i]:
+                    newIndex = i
+                    break
+
+            if newIndex == -1:
+                newIndex = len(ordering)
+        if item.value not in ordering:
+            ordering.insert(newIndex, item.value)
+    
+    allAfters = [x for item in items for x in item.afters]
+    filtered = list(dict.fromkeys([x for x in allAfters if x not in ordering]))
+    ordering += filtered
+
+    for item in items:
+        print(item.value, item.afters)
+    print(ordering)
 
 part1()
