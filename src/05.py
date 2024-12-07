be5e37f0066047418a786d9f20a4f3350ec2a3ed
Antti-Jussi Lakanen <85225433+ajlakanen@users.noreply.github.com>
2024-12-07 08:32:56 +0000

    remove crap

--- src/05.py
+++ src/05.py
@@ -1,11 +1,8 @@
-import functools
-import itertools
 import math
-import pytest
 
 # Read the file
 
-file = open("data/05-example.txt", "r")
+file = open("data/05.txt", "r")
 content = file.read()
 lines = content.split("\n")
 
@@ -90,12 +87,6 @@ class Graph:
         return self.count_from(x)
 
 
-class Item:
-    """Class for items in the ordering list"""
-    def __init__(self, value, afters):
-        self.value = value
-        self.afters = afters
-
 
 def isUpdateValid(update, ordering):
     """Is the update valid according to the ordering list"""
@@ -124,59 +115,16 @@ def part1():
             g.add_node(rule[1])
         
         g.add_edge(rule[0], rule[1])
-## 
-    ##     # if len(list(filter(lambda item: item.value == rule[0], items))) == 0:
-    ##     #     items.append(Item(rule[0], [rule[1]]))
-    ##     # Otherwise update the "afters" list
-    ##     #else:
-    ##     #    item = list(filter(lambda item: item.value == rule[0], items))[0]
-    ##     #    item.afters.append(rule[1])
-## 
-    ## ordering = [items[0].value]
-## 
-    ## # Go through all items
-    ## for item in items[1:]:
-    ##     # Find the index where the item should be inserted
-    ##     # Start with the assumption that the item should be inserted at the end of the list
-    ##     newIndex = len(ordering)
-## 
-    ##     # Go through all items in the ordering list
-    ##     for i in range(len(ordering)): # tsekkaa mitä tapahtuu kun item.value == 66
-    ##         # Go through the "afters" list of the item and check if the ccurrent element is in the list
-    ##         # These are the values that should come after the item
-    ##         # There are surely more efficient ways to do this, I haven't had time to think about it
-    ##         for j in range(len(item.afters)):
-    ##             if ordering[i] == item.afters[j]:
-    ##             #if item.afters[j] == ordering[i]:
-    ##                 if i < newIndex:
-    ##                     newIndex = i
-    ##                 break
-    ##         
-    ##     # Only insert if not already in the list
-    ##     if item.value not in ordering:
-    ##         ordering.insert(newIndex, item.value)
-
-    # If "afters" list contains values not in the ordering list, add them to the ordering.
-    # I don't think it matters if they are added in the correct order
-    ## allAfters = [x for item in items for x in item.afters]
-    ## filtered = list(dict.fromkeys([x for x in allAfters if x not in ordering]))
-    ## ordering += filtered
-
     ordering = g.createTopologicalOrder()
-
-    # For debugging: 
-    # for item in items:
-    #     print(item.value, item.afters)
-    # print(ordering)
 
     valids = []
     for update in updates:
         if isUpdateValid(update, ordering):
-            # print(update, "valid")
+            print(update, "valid")
             valids.append(update)
         else:
             pass
-            # print(update, "invalid")
+            print(update, "invalid")
 
     sum = 0
     for valid in valids:
