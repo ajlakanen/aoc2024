045dfdbd52769528cf78d6763a5589271da28193
anlakane <anlakane@jyu.fi>
2024-12-13 15:10:16 +0200

    part 2 wip

--- src/13.py
+++ src/13.py
@@ -1,7 +1,7 @@
 import re 
 import numpy as np
 
-file = open("data/13.txt", "r")
+file = open("data/13-example.txt", "r")
 content = file.read()
 lines = content.split("\n")
 
@@ -25,19 +25,35 @@ while i < len(lines):
     i += 2
     machines.append(Machine(a,b,prize))
 
-sum = 0
-for machine in machines:
-    # print(machine.a, machine.b, machine.price)
-    A = np.array([[machine.a[0], machine.b[0]], [machine.a[1], machine.b[1]]])
-    y = np.array([machine.price[0], machine.price[1]])
-    x = np.linalg.solve(A, y)
-    if x[0] <= 0 or x[1] <= 0:
-        continue
-    if x[0] >= 100 or x[1] >= 100:
-        continue
+def part1():
+    sum = 0
+    for machine in machines:
+        A = np.array([[machine.a[0], machine.b[0]], [machine.a[1], machine.b[1]]])
+        y = np.array([machine.price[0], machine.price[1]])
+        x = np.linalg.solve(A, y)
+        if x[0] <= 0 or x[1] <= 0:
+            continue
+        if x[0] >= 100 or x[1] >= 100:
+            continue
 
-    print(x)
-    if all(list(map(lambda x: round(x, 7).is_integer(), x))):
-        sum += 3 * x[0] + x[1]
-    
-print(sum)
+        # print(x)
+        if all(list(map(lambda x: round(x, 7).is_integer(), x))):
+            sum += 3 * x[0] + x[1]    
+    print(sum)
+
+def part2():
+    sum = 0
+    for machine in machines:
+        A = np.array([[machine.a[0], machine.b[0]], [machine.a[1], machine.b[1]]])
+        y = np.array([10000000000000 + machine.price[0], 10000000000000 + machine.price[1]])
+        x = np.linalg.solve(A, y)
+        print(A, y, x)
+        if x[0] <= 0 or x[1] <= 0:
+            continue
+        if all(list(map(lambda x: round(x, 7).is_integer(), x))):
+            sum += 3 * x[0] + x[1]
+    print(sum)
+
+
+part1()
+part2()
